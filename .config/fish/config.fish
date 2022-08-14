23da2d35e866427534960124bd292c3ab76df7c6
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-08-14 19:28:07 +0200

    fix path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,11 @@
 abbr ytdm       'youtube-dl -f bestaudio -i --embed-thumbnail --no-playlist --add-metadata -x --audio-format mp3 -o "./%(title)s.%(ext)s"'
 abbr ytdv       'youtube-dl -i --embed-thumbnail --add-metadata -o "./%(title)s.%(ext)s"'
-abbr vim        'lvim'
-abbr nvim       'lvim'
 abbr mkdir      'mkdir -p'
-abbr trash      'gio trash'
 abbr rm         'rm -I'
 abbr cp         'cp -riv'
 abbr ffcut      'ffmpeg -i in.mp4 -ss 00:00:00 -to 00:00:00 -async 1 out.mp4'
-abbr weather	  'curl wttr.in/Sępopol'
-abbr wlp	      'systemctl --user start wallpaper-changer.service'
 abbr du	        'du -sh'
-abbr lgscan	    'loudgain --tagmode=e --lowercase --track --noclip'
+abbr lgscan	'loudgain --tagmode=e --lowercase --track --noclip'
 abbr paru       'paru --noconfirm'
 abbr flatpaki   'flatpak --assumeyes install'
 abbr copy       'xclip -selection clipboard'
@@ -34,3 +29,5 @@ end
 zoxide init fish | source
 alias cd '__zoxide_z'
 
+
+fenv source .profile

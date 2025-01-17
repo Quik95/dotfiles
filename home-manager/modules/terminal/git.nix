a96eb942d887bf8e8d07df0eda01ba97649ae6f2
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-01-18 00:01:27 +0100

    Use difftastic

--- home-manager/modules/terminal/git.nix
+++ home-manager/modules/terminal/git.nix
@@ -4,8 +4,20 @@
     userName = "Sebastian Bartoszewicz";
     userEmail = "quikstyletv@gmail.com";
 
+    difftastic.enable = true;
+
     extraConfig = {
       init.defaultBranch = "master";
+      diff.colorMoved = "zebra";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
     };
+
+    ignores = [
+      "*~"
+      ".DS_Store"
+    ];
   };
+
+  programs.gitui.enable = true;
 }

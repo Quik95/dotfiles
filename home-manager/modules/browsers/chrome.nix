641fd4cc42381d43d6cf88e39ed9167275b27e71
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-01-24 19:16:47 +0100

    Revert "Remove Google Chrome flags config"
    This reverts commit bb138313216c979e749f03ddffda30ba49afebf7.

---
+++ home-manager/modules/browsers/chrome.nix
@@ -0,0 +1,19 @@
+{config, lib, ...}: 
+let 
+  flags = ''
+    --gtk-version=4
+    --ignore-gpu-blocklist
+    --enable-features=UseOzonePlatform,TouchGestures
+    --ozone-platform=wayland
+    --enable-gpu-rasterization
+    --enable-experimental-web-platform-features
+    --ozone-platform-hint=auto
+    --enable-features=TouchpadOverscrollHistoryNavigation
+    '';
+in {
+  # We have to do this that way, because chrome doesn't have access to files in the nix store
+  home.activation.createChromeFlags = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p "${config.home.homeDirectory}/.var/app/com.google.Chrome/config"
+    echo "${flags}" > ${config.home.homeDirectory}/.var/app/com.google.Chrome/config/chrome-flags.conf
+  '';
+}
--- home-manager/modules/browsers/default.nix
+++ home-manager/modules/browsers/default.nix
@@ -1,4 +1,5 @@
 {
   imports = [
+    ./chrome.nix
   ];
 }

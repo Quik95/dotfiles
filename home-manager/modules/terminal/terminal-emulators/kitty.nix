d2feb025e63029dcbae2ce6cfb2d5c8335c752bd
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-01-26 12:50:37 +0100

    Enable kitty terminal

--- home-manager/modules/terminal/terminal-emulators/kitty.nix
+++ home-manager/modules/terminal/terminal-emulators/kitty.nix
@@ -1,6 +1,6 @@
 {
   programs.kitty = {
-    enable = false;
+    enable = true;
     shellIntegration.enableFishIntegration = true;
     themeFile = "Catppuccin-Frappe";
     font = {
@@ -8,16 +8,17 @@
       name = "DejaVuSansMono";
     };
     settings = {
-      "disable_ligatures" = "never";
-      "scrollback_lines" = 1000000;
-      "scrollback_pager" = "bat";
-      "copy_on_select" = "no";
-      "paste_actions" = "quote-urls-at-prompt";
-      "strip_trailing_spaces" = "smart";
-      "sync_to_monitor" = "yes";
-      "enable_audio_bell" = "no";
-      "tab_bar_style" = "powerline";
-      "background_opacity" = 0.9;
+      disable_ligatures = "never";
+      scrollback_lines = 1000000;
+      scrollback_pager = "bat";
+      copy_on_select = "no";
+      paste_actions = "quote-urls-at-prompt";
+      strip_trailing_spaces = "smart";
+      sync_to_monitor = "yes";
+      enable_audio_bell = "no";
+      tab_bar_style = "powerline";
+      background_opacity = 0.9;
+      cursor_trail = 1;
     };
   };
 }

97af715ac1c61b6b9f086850f9f52ef72ee7328c
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-02-14 21:39:39 +0100

    Set fish as the default shell

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -8,6 +8,19 @@
   services.xserver.excludePackages = [pkgs.xterm];
   services.xserver.desktopManager.xterm.enable = false;
 
+  programs.fish.enable = true;
+  environment.shells = [ pkgs.bashInteractive pkgs.fish ];
+  users.defaultUserShell = pkgs.fish;
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
   environment.systemPackages =
     (with pkgs; [
       git

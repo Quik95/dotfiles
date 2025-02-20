ae6f23d1a32ca1076991032dc60410f63e9db680
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-02-20 22:00:48 +0100

    Store gitlab ssh key in sops

--- home-manager/modules/sops.nix
+++ home-manager/modules/sops.nix
@@ -7,6 +7,16 @@
         sopsFile = ../secrets/eduroam-certificate.bin;
         path = "${config.xdg.configHome}/certs/eduroam.p12";
       };
+
+      gitlab-public-key = {
+        sopsFile = ../secrets/gitlab-ssh-key.yaml;
+        path = "${config.home.homeDirectory}/.ssh/gitlab_key.pub";
+      };
+
+      gitlab-private-key = {
+        sopsFile = ../secrets/gitlab-ssh-key.yaml;
+        path = "${config.home.homeDirectory}/.ssh/gitlab_key";
+      };
     };
   };
 }

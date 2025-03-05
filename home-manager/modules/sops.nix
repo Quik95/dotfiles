5348921e9a083842d9369222a49a15ed16fadafb
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-03-05 22:53:29 +0100

    Add ssh and gpg keys for git.cs.put.poznan.pl

--- home-manager/modules/sops.nix
+++ home-manager/modules/sops.nix
@@ -18,6 +18,26 @@
         path = "${config.home.homeDirectory}/.ssh/gitlab_key";
       };
 
+      gitlab-cs-put-public-key = {
+        sopsFile = ../secrets/gitlab-cs-put-ssh-key.yaml;
+        path = "${config.home.homeDirectory}/.ssh/gitlab_cs_put_key.pub";
+      };
+
+      gitlab-cs-put-private-key = {
+        sopsFile = ../secrets/gitlab-cs-put-ssh-key.yaml;
+        path = "${config.home.homeDirectory}/.ssh/gitlab_cs_put_key";
+      };
+
+      gitlab-cs-put-gpg-public-key = {
+        sopsFile = ../secrets/gitlab-cs-put-gpg-key.yaml;
+        path = "${config.home.homeDirectory}/.gnupg/gitlab-cs-put-public-key.asc";
+      };
+
+      gitlab-cs-put-gpg-private-key = {
+        sopsFile = ../secrets/gitlab-cs-put-gpg-key.yaml;
+        path = "${config.home.homeDirectory}/.gnupg/gitlab-cs-put-private-key.asc";
+      };
+
       github-public-key = {
         sopsFile = ../secrets/github-ssh-key.yaml;
         path = "${config.home.homeDirectory}/.ssh/github_key.pub";

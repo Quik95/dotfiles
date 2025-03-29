a703c3192961a49ad314118bc1603031311a428f
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-03-29 11:30:47 +0100

    Install additional IDE

--- home-manager/modules/ide/jetbrains.nix
+++ home-manager/modules/ide/jetbrains.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: let
-  standardPlugins = ["github-copilot" "ideavim"];
+  standardPlugins = ["github-copilot" "ideavim" "nixidea" "which-key"];
 in {
   home.packages = with pkgs; [
     (jetbrains.plugins.addPlugins jetbrains.clion standardPlugins)
     (jetbrains.plugins.addPlugins jetbrains.rust-rover standardPlugins)
+    (jetbrains.plugins.addPlugins jetbrains.pycharm-professional standardPlugins)
+    (jetbrains.plugins.addPlugins jetbrains.webstorm standardPlugins)
   ];
 
   home.file.".ideavimrc".source = ./.ideavimrc;

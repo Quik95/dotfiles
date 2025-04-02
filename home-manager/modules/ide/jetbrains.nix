e7fb51f38bde3fa9930822a3d0484c380a5b0cd8
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-04-03 00:20:27 +0200

    Install jetbrains IDEs

--- home-manager/modules/ide/jetbrains.nix
+++ home-manager/modules/ide/jetbrains.nix
@@ -2,7 +2,9 @@
   standardPlugins = ["github-copilot" "ideavim" "nixidea" "which-key"];
 in {
   home.packages = with pkgs; [
-    
+    (jetbrains.plugins.addPlugins jetbrains.clion standardPlugins)
+    (jetbrains.plugins.addPlugins jetbrains.pycharm-professional standardPlugins)
+    (jetbrains.plugins.addPlugins jetbrains.webstorm standardPlugins)
   ];
 
   home.file.".ideavimrc".source = ./.ideavimrc;

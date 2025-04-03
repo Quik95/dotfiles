01cca3343eb529b8afd9ec0ab69afb8796627437
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-04-03 15:57:27 +0200

    Set bookmarks for nautilus

---
+++ home-manager/modules/wm/gnome/nautilus.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  nautilusBookmarks = [
+    { path = "${config.home.homeDirectory}/Documents"; name = "Documents"; }
+    { path = "${config.home.homeDirectory}/Downloads"; name = "Downloads"; }
+    { path = "${config.home.homeDirectory}/Music"; name = "Music"; }
+    { path = "${config.home.homeDirectory}/Pictures"; name = "Pictures"; }
+    { path = "${config.home.homeDirectory}/Videos"; name = "Videos"; }
+    { path = "${config.home.homeDirectory}/Projects/Studia/Magisterka/Semestr_I"; name = "Semestr I"; }
+    { path = "/tmp"; name = "Temp Dir"; }
+  ];
+  formatBookmark = bookmark: "file://${bookmark.path} ${bookmark.name}";
+  bookmarksFileContents = lib.concatStringsSep "\n" (
+    lib.map formatBookmark nautilusBookmarks
+  );
+in {
+  xdg.configFile."gtk-3.0/bookmarks" = {
+    force = true;
+    text = bookmarksFileContents;
+  };
+}

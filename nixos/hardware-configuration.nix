e9b89cf1d349b4d83e0944ad95c2003652ad5f2d
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-05-14 23:49:24 +0200

    Disable mitigations

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -10,6 +10,9 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelParams = [
+    "mitigations=off"
+  ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 

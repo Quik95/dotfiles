589033cc65348b0ee4f8938ca6ea1c3bffee2da2
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-06-16 09:37:22 +0200

    Remove unused IDEs

--- home-manager/modules/ide/jetbrains.nix
+++ home-manager/modules/ide/jetbrains.nix
@@ -4,9 +4,6 @@
 in {
   home.packages = with pkgs; [
     (jetbrains.plugins.addPlugins jetbrains.clion standardPlugins)
-    (jetbrains.plugins.addPlugins jetbrains.pycharm-professional standardPlugins)
-    (jetbrains.plugins.addPlugins jetbrains.webstorm standardPlugins)
-    (jetbrains.plugins.addPlugins jetbrains.rider standardPlugins)
     (jetbrains.plugins.addPlugins jetbrains.rust-rover standardPlugins)
     (jetbrains.plugins.addPlugins jetbrains.idea-ultimate standardPlugins)
   ];

faa9c5c8cb6c7cdafbb4b54ba546a0fe35b3dca4
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-06-17 23:38:13 +0200

    Use base24 themes

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,8 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  tt-schemes = pkgs.fetchFromGitHub {
+    owner = "tinted-theming";
+    repo = "schemes";
+    rev = "b15ea410ff2091a064a92d0f6b8bae80a2f27798";
+    hash = "sha256-pDz3SALMXwLvqvVPKj2pQn1Cr6WsPTWICaUhWfmXAYI=";
+  };
+in {
   stylix = {
     enable = true;
     autoEnable = false;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/digital-rain.yaml";
+    base16Scheme = "${tt-schemes}/base24/purple-rain.yaml";
     targets = {
       bat.enable = true;
       btop.enable = true;
@@ -12,7 +19,7 @@
       kde.enable = true;
       kitty.enable = true;
       lazygit.enable = true;
-      mpv.enable  = true;
+      mpv.enable = true;
     };
   };
 }

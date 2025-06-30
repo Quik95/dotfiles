6d4fca8c465ccd1454b2d1275a2a17f946f17e4e
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-06-30 18:26:11 +0200

    Revert "Configure monitor layout"
    This reverts commit 62ebda516ea5290e146bdac8203dd3a3c3150cc1.

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,4 +1,8 @@
+# Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
+# These should be stuff you would like to share with others, not your personal configurations.
 {
+  # List your module files here
+  # my-module = import ./my-module.nix;
   imports = [
     ./browsers/default.nix
     ./ide/default.nix
@@ -12,7 +16,6 @@
     ./env-vars.nix
     ./flatpak.nix
     ./misc.nix
-    ./monitors.nix
     ./sops.nix
     ./ssh.nix
     ./stylix.nix

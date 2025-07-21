55d2694b4ae8fff18ffcefcf59b1b853bfd60f5b
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-07-21 20:02:39 +0200

    Install only required treesitter grammars

--- home-manager/modules/ide/nixvim/plugins/treesitter.nix
+++ home-manager/modules/ide/nixvim/plugins/treesitter.nix
@@ -1,48 +1,7 @@
-{
+{pkgs, ...}: {
   programs.nixvim.plugins.treesitter = {
     enable = true;
     settings = {
-      ensureInstalled = [
-        "angular"
-        "bash"
-        "bibtex"
-        "c"
-        "c_sharp"
-        "cpp"
-        "css"
-        "csv"
-        "diff"
-        "dockerfile"
-        "fish"
-        "git_config"
-        "git_rebase"
-        "gitattributes"
-        "gitcommit"
-        "gitignore"
-        "html"
-        "http"
-        "javascript"
-        "json"
-        "json5"
-        "kotlin"
-        "latex"
-        "lua"
-        "luadoc"
-        "make"
-        "markdown"
-        "markdown_inline"
-        "meson"
-        "python"
-        "regex"
-        "rust"
-        "scss"
-        "sql"
-        "toml"
-        "typescript"
-        "vim"
-        "vimdoc"
-      ];
-
       highlight = {
         enable = true;
         additional_vim_regex_highlighting = true;
@@ -50,5 +9,47 @@
 
       indent.enable = true;
     };
+
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      angular
+      bash
+      bibtex
+      c
+      c_sharp
+      cpp
+      css
+      csv
+      diff
+      dockerfile
+      fish
+      git_config
+      git_rebase
+      gitattributes
+      gitcommit
+      gitignore
+      html
+      http
+      javascript
+      json
+      json5
+      kotlin
+      latex
+      lua
+      luadoc
+      make
+      markdown
+      markdown_inline
+      meson
+      nix
+      python
+      regex
+      rust
+      scss
+      sql
+      toml
+      typescript
+      vim
+      vimdoc
+    ];
   };
 }

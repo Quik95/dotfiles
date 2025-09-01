ab65e924a2edf1417fa0216324d622d3f808e630
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-09-01 23:53:40 +0200

    Update jetbrains ide

--- home-manager/modules/ide/jetbrains.nix
+++ home-manager/modules/ide/jetbrains.nix
@@ -3,26 +3,12 @@
   config,
   ...
 }: let
-  jetbrains = (import
-    (pkgs.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "94def634a20494ee057c76998843c015909d6311";
-        hash = "sha256-K2ViRJfdVGE8tpJejs8Qpvvejks1+A4GQej/lBk5y7I=";
-      }
-    )
-    {
-      inherit (config.nixpkgs) config;
-      localSystem = {
-        system = "x86_64-linux";
-      };
-    }
-  ).jetbrains;
+  jetbrains = pkgs.jetbrains;
   # standardPlugins = ["github-copilot" "ideavim" "nixidea" "which-key"];
   standardPlugins = ["ideavim" "nixidea"];
 in {
   home.packages = [
-    (jetbrains.plugins.addPlugins jetbrains.clion standardPlugins)
+    (jetbrains.plugins.addPlugins jetbrains.webstorm standardPlugins)
     (jetbrains.plugins.addPlugins jetbrains.rust-rover standardPlugins)
     (jetbrains.plugins.addPlugins jetbrains.datagrip standardPlugins)
   ];

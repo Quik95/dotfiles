6ddda84ab39c6cfca6f113dc82528d47e1f28ad1
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-09-02 23:43:14 +0200

    Update the system

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,7 +1,19 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "ask";
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "ask";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "${config.home.homeDirectory}/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
   };
 
   services.ssh-agent = {

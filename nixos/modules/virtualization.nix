2b8846eed6775540d4879566093973e401902364
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-10-04 16:33:16 +0200

    Install k3s for Kubernetes support

--- nixos/modules/virtualization.nix
+++ nixos/modules/virtualization.nix
@@ -6,12 +6,19 @@
     podman = {
       enable = true;
       dockerCompat = true;
+      dockerSocket.enable = true;
       autoPrune.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
 
+  services.k3s = {
+    enable = true;
+    role = "server";
+  };
+
   environment.systemPackages = with pkgs; [
     podman-compose
+    k3d
   ];
 }

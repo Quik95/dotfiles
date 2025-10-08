1d517a72c430cacb2e812dc5447e5b7de96664de
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-10-08 23:42:23 +0200

    Enable git integration

--- home-manager/modules/terminal/terminal-emulators/kitty.nix
+++ home-manager/modules/terminal/terminal-emulators/kitty.nix
@@ -2,15 +2,16 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
+    enableGitIntegration = true;
     settings = {
+      copy_on_select = "no";
       disable_ligatures = "never";
+      enable_audio_bell = "no";
+      paste_actions = "quote-urls-at-prompt";
       scrollback_lines = 1000000;
       scrollback_pager = "bat";
-      copy_on_select = "no";
-      paste_actions = "quote-urls-at-prompt";
       strip_trailing_spaces = "smart";
       sync_to_monitor = "yes";
-      enable_audio_bell = "no";
       tab_bar_style = "powerline";
     };
   };

5e4fa695b07c9071eb86ad15f1f1183643aa7dc8
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2025-11-06 18:38:24 +0100

    Adjust the firefox configuration

--- home-manager/modules/browsers/firefox.nix
+++ home-manager/modules/browsers/firefox.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-beta;
+    package = pkgs.firefox-bin;
     languagePacks = ["en-GB" "pl"];
     policies = {
       DefaultDownloadDirectory = "/tmp";
@@ -81,7 +81,7 @@
             iconMapObj."16" = "https://mycroftproject.com/favicon.ico";
             urls = [
               {
-                template = "https://mycroftproject.com/search-engines.html?name={searchTerms}";
+                template = "https://mycroftproject.com/search-engines.html";
                 params = [
                   {
                     name = "name";
@@ -97,8 +97,12 @@
             iconMapObj."16" = "https://mycroftproject.com/updateos.php/id0/archlinux_wiki_ssl.png";
             urls = [
               {
-                template = "https://wiki.archlinux.org/index.php/Special:Search?go=Go&amp;search={searchTerms}";
+                template = "https://wiki.archlinux.org/index.php/Special:Search";
                 params = [
+                  {
+                    name = "go";
+                    value = "Go";
+                  }
                   {
                     name = "search";
                     value = "{searchTerms}";
@@ -113,11 +117,47 @@
             iconMapObj."16" = "https://mycroftproject.com/updateos.php/id0/github93het.ico";
             urls = [
               {
-                template = "https://github.com/search?q={searchTerms}";
+                template = "https://github.com/search";
                 params = [
                   {
-                    name = "search";
-                    value = "{searchParams}";
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+          "nixpkgs-unstable" = {
+            name = "NixOS Packages (unstable)";
+            definedAliases = ["@nix"];
+            iconMapObj."16" = "https://mycroftproject.com/updateos.php/id0/nixpkgs-unstable-ver.ico";
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "channel";
+                    value = "unstable";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+          "nixpkgs-pr" = {
+            name = "NixOS nixpkgs Pull Requests";
+            definedAliases = ["@nixpr"];
+            iconMapObj."16" = "https://mycroftproject.com/updateos.php/id0/github93het.ico";
+            urls = [
+              {
+                template = "https://github.com/NixOS/nixpkgs/pulls";
+                params = [
+                  {
+                    name = "q";
+                    value = "sort:updated-desc is:pr is:open {searchTerms}";
                   }
                 ];
               }
@@ -127,11 +167,43 @@
       };
 
       settings = {
+        # General settings
         "browser.shell.checkDefaultBrowser" = false;
         "browser.startup.homepage" = "about:blank";
         "browser.urlbar.trimURLs" = true;
         "browser.search.suggest.enabled" = true;
         "ui.key.menuAccessKey" = 0;
+
+        # Performance
+        "gfx.webrender.all" = true;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+
+        # Wayland-specific improvements
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "widget.use-xdg-desktop-portal.mime-handler" = 1;
+
+        # Security
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
+
+        # UI improvements
+        "browser.tabs.loadInBackground" = true;
+        "browser.tabs.closeWindowWithLastTab" = false;
+        "browser.tabs.warnOnClose" = false;
+        "browser.tabs.warnOnCloseOtherTabs" = false;
+        "browser.download.useDownloadDir" = false;
+        "browser.toolbars.bookmarks.showOtherBookmarks" = false;
+        "sidebar.visibility" = "hide-sidebar-button";
+
+        # Address bar suggestions
+        "browser.urlbar.suggest.bookmark" = true;
+        "browser.urlbar.suggest.history" = true;
+        "browser.urlbar.suggest.openpage" = true;
+        "browser.urlbar.suggest.topsites" = true;
+        "browser.urlbar.suggest.engines" = true;
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
       };
     };
   };

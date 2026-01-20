e4ed04cd3b436d1847cddc61b8f4c7ab0bf313bb
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-01-20 22:09:00 +0100

    Add skill for typst

--- home-manager/modules/ai/opencode.nix
+++ home-manager/modules/ai/opencode.nix
@@ -4,6 +4,13 @@
   ...
 }: let
   skillsDirectory = "${config.xdg.configHome}/opencode/skills";
+
+  typst-author = pkgs.fetchFromGitHub {
+    owner = "apcamargo";
+    repo = "typst-author";
+    rev = "9e4ace023b255ffbd9dacd26fe27665eef9c6d4b";
+    hash = "sha256-FCr+Cgi+mI9H2dtEgtrH93aj3hfolKhoP71EYiFOglo=";
+  };
 in {
   home.sessionVariables = {
     OPENCODE_DISABLE_CLAUDE_CODE = 1;
@@ -12,6 +19,11 @@ in {
   home.file."${skillsDirectory}/bash-expert/SKILL.md".source = ./skills/bash.md;
   home.file."${skillsDirectory}/dotnet-core-expert/SKILL.md".source = ./skills/dotnet.md;
   home.file."${skillsDirectory}/powershell-expert/SKILL.md".source = ./skills/powershell.md;
+  home.file."${skillsDirectory}/typst-author/SKILL.md".source = "${typst-author}/SKILL.md";
+  home.file."${skillsDirectory}/typst-author/docs" = {
+    source = "${typst-author}/docs";
+    recursive = true;
+  };
 
   programs.opencode = {
     enable = true;

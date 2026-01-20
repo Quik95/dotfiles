08713b3a453bee075f89e5fc6cc40b96c01ddf8c
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-01-20 20:46:11 +0100

    Add skills and mcp config to claude-code

--- home-manager/modules/ai/claudecode.nix
+++ home-manager/modules/ai/claudecode.nix
@@ -1,5 +1,15 @@
 {
+  config,
+  ...
+}: let
+  skillsDirectory = "${config.home.homeDirectory}/.claude/skills";
+in {
+  home.file."${skillsDirectory}/bash-expert/SKILL.md".source = ./skills/bash.md;
+  home.file."${skillsDirectory}/dotnet-core-expert/SKILL.md".source = ./skills/dotnet.md;
+  home.file."${skillsDirectory}/powershell-expert/SKILL.md".source = ./skills/powershell.md;
+
   programs.claude-code = {
     enable = true;
+    enableMcpIntegration = true;
   };
 }

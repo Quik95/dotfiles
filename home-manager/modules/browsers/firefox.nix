c5611a01c962cd3bc989fc3ee795164308dda7a0
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-02-01 00:55:42 +0100

    Configure firefox navbar

--- home-manager/modules/browsers/firefox.nix
+++ home-manager/modules/browsers/firefox.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: let
+  dictionaries = with pkgs.hunspellDicts; [
+    pl_PL
+    en_GB-large
+  ];
+in {
   home.packages = with pkgs; [
     hunspell
-    hunspellDicts.pl_PL
-  ];
+  ] ++ dictionaries;
+
+  home.sessionVariables = {
+    DICPATH = lib.concatMapStringsSep ":" (dict: "${dict}/share/hunspell") dictionaries;
+  };
 
   programs.firefox = {
     enable = true;
@@ -72,6 +80,77 @@
         "browser.urlbar.suggest.quicksuggest.sponsored" = false;
         "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
 
+        # Navbar
+        "browser.uiCustomization.state" = {
+          placements = {
+            "widget-overflow-fixed-list" = [];
+            "unified-extensions-area" = [
+              "gdpr_cavi_au_dk-browser-action"
+              "myallychou_gmail_com-browser-action"
+              "_7be2ba16-0f1e-4d93-9ebc-5164397477a9_-browser-action"
+              "_a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad_-browser-action"
+              "wappalyzer_crunchlabz_com-browser-action"
+              "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+              "_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action"
+              "_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action"
+            ];
+            "nav-bar" = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "vertical-spacer"
+              "customizableui-special-spring1"
+              "urlbar-container"
+              "customizableui-special-spring2"
+              "downloads-button"
+              "screenshot-button"
+              "unified-extensions-button"
+              "fxa-toolbar-menu-button"
+              "reset-pbm-toolbar-button"
+              "ublock0_raymondhill_net-browser-action"
+              "myallychou_gmail_com-browser-action"
+              "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+            ];
+            "toolbar-menubar" = [
+              "menubar-items"
+            ];
+            "TabsToolbar" = [
+              "firefox-view-button"
+              "tabbrowser-tabs"
+              "new-tab-button"
+              "alltabs-button"
+            ];
+            "vertical-tabs" = [];
+            "PersonalToolbar" = [
+              "personal-bookmarks"
+            ];
+          };
+          seen = [
+            "gdpr_cavi_au_dk-browser-action"
+            "myallychou_gmail_com-browser-action"
+            "_7be2ba16-0f1e-4d93-9ebc-5164397477a9_-browser-action"
+            "_a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad_-browser-action"
+            "ublock0_raymondhill_net-browser-action"
+            "wappalyzer_crunchlabz_com-browser-action"
+            "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+            "_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action"
+            "jid0-adyhmvsp91nuo8prv0mn2vkeb84_jetpack-browser-action"
+            "sponsorblocker_ajay_app-browser-action"
+            "_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action"
+            "developer-button"
+            "screenshot-button"
+          ];
+          dirtyAreaCache = [
+            "unified-extensions-area"
+            "nav-bar"
+            "vertical-tabs"
+            "toolbar-menubar"
+            "TabsToolbar"
+            "PersonalToolbar"
+          ];
+          currentVersion = 23;
+          newElementCount = 2;
+        };
         # Spellcheck
         "spellcheck.dictionary" = "pl,en-GB";
         "layout.spellcheckDefault" = 2;

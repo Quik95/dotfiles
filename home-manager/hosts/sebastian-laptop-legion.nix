59f8c979f8f043c1d7dd7f9acff154a03ad0a16a
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-03-23 23:14:49 +0100

    Adjust resolution settings

--- home-manager/hosts/sebastian-laptop-legion.nix
+++ home-manager/hosts/sebastian-laptop-legion.nix
@@ -1,4 +1,8 @@
 {lib, ...}: {
+  programs.mpv.config = {
+    ytdl-format = lib.mkForce "(bestvideo[height<=1600][vcodec*=h264]+bestaudio)/(bestvideo[height<=1600][vcodec!*=vp9]+bestaudio)/(bestvideo[height<=1600]+bestaudio)/best[height<=1600]";
+  };
+
   dconf.settings = {
     # OLED protection: blank screen after 3 minutes of inactivity
     "org/gnome/desktop/session" = {

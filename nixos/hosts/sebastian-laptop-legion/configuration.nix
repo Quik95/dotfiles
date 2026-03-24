4af49cb19f6ffa4d4ab634bb3cc983ae136b7e4a
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-03-24 20:43:14 +0100

    Włączenie systemd-oom killer

--- nixos/hosts/sebastian-laptop-legion/configuration.nix
+++ nixos/hosts/sebastian-laptop-legion/configuration.nix
@@ -4,6 +4,7 @@
     ./gpu.nix
     ./gpu-specialisations.nix
     ./bluetooth.nix
+    ./swap.nix
   ];
 
   nixfiles.enable = true;
@@ -37,33 +38,7 @@
     mode = 1;
   };
 
-  boot.kernelParams = [
-    "amd_pstate=active"
-    "zswap.enabled=1"
-    "zswap.compressor=zstd"
-    "zswap.zpool=zsmalloc"
-    "zswap.shrinker_enabled=1"
-  ];
-
-  fileSystems."/swap" = {
-    device = "/dev/disk/by-uuid/332b254c-7d52-4899-bfda-4207bac52c49";
-    fsType = "btrfs";
-    options = ["subvol=@swap"];
-  };
-
-  swapDevices = lib.mkForce [{
-    device = "/swap/swapfile";
-  }];
-
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 100;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
-    "vm.page-cluster" = 3;
-    "vm.vfs_cache_pressure" = 125;
-    "vm.dirty_ratio" = 10;
-    "vm.dirty_background_ratio" = 5;
-  };
+  boot.kernelParams = ["amd_pstate=active"];
 
   services.logind.settings.Login.HandleLidSwitch = "suspend";
 

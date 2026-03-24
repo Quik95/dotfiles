252d704b60d19b0ab8f1a897840271e7bbb8468f
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-03-24 20:40:35 +0100

    Change zram to zswap

--- nixos/hosts/sebastian-laptop-legion/configuration.nix
+++ nixos/hosts/sebastian-laptop-legion/configuration.nix
@@ -37,21 +37,29 @@
     mode = 1;
   };
 
-  boot.kernelParams = ["amd_pstate=active"];
+  boot.kernelParams = [
+    "amd_pstate=active"
+    "zswap.enabled=1"
+    "zswap.compressor=zstd"
+    "zswap.zpool=zsmalloc"
+    "zswap.shrinker_enabled=1"
+  ];
 
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 100;
+  fileSystems."/swap" = {
+    device = "/dev/disk/by-uuid/332b254c-7d52-4899-bfda-4207bac52c49";
+    fsType = "btrfs";
+    options = ["subvol=@swap"];
   };
 
-  swapDevices = lib.mkForce [];
+  swapDevices = lib.mkForce [{
+    device = "/swap/swapfile";
+  }];
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180;
+    "vm.swappiness" = 100;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
-    "vm.page-cluster" = 0;
+    "vm.page-cluster" = 3;
     "vm.vfs_cache_pressure" = 125;
     "vm.dirty_ratio" = 10;
     "vm.dirty_background_ratio" = 5;

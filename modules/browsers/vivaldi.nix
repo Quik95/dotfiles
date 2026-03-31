ef6251854295764681c0c49214587a37a4ceb1d6
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-03-31 15:23:13 +0200

    Enable wayland in Vivaldi

--- modules/browsers/vivaldi.nix
+++ modules/browsers/vivaldi.nix
@@ -3,16 +3,29 @@
   lib,
   ...
 }: let
+  features = [
+    "UseOzonePlatform"
+    "TouchGestures"
+    "TouchpadOverscrollHistoryNavigation"
+    "VaapiVideoDecoder"
+    "AcceleratedVideoDecodeLinuxGL"
+    "AcceleratedVideoDecodeLinuxZeroCopyGL"
+    "AcceleratedVideoEncoder"
+    "UseMultiPlaneFormatForHardwareVideo"
+  ];
+
   vivaldiWrapped = pkgs.vivaldi.override {
     proprietaryCodecs = true;
     enableWidevine = true;
     commandLineArgs = lib.concatStringsSep " " [
+      "--ozone-platform=wayland"
       "--ozone-platform-hint=auto"
       "--enable-wayland-ime"
       "--ignore-gpu-blocklist"
       "--enable-gpu-rasterization"
       "--enable-zero-copy"
-      "--enable-features=VaapiVideoDecoder,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoEncoder,UseMultiPlaneFormatForHardwareVideo"
+      "--enable-features=${lib.concatStringsSep "," features}"
+      "--disable-features=GlobalShortcutsPortal"
     ];
   };
 in {

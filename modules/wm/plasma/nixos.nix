2d621376e9d50a7e118bca5bf0243d11c14d4429
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-04-22 23:32:36 +0200

    Remove system updates notifications

--- modules/wm/plasma/nixos.nix
+++ modules/wm/plasma/nixos.nix
@@ -7,6 +7,7 @@ lib.mkIf (config.networking.hostName == "sebastian-laptop-legion") {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  services.packagekit.enable = false;
 
   programs.dconf.enable = true;
 

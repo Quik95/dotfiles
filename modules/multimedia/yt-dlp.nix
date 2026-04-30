f2a163384daa17e49e550cfdbdc469528f1fb4db
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2026-04-30 23:00:21 +0200

    Use cookies from firefox

--- modules/multimedia/yt-dlp.nix
+++ modules/multimedia/yt-dlp.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{...}: {
   programs.yt-dlp = {
     enable = true;
     settings = {
@@ -7,7 +7,7 @@
       sub-langs = "en.*,pl.*";
       write-sub = true;
       format = "(bestvideo[height<=1080][vcodec*=h264]+bestaudio)/(bestvideo[height<=1080][vcodec!*=vp9]+bestaudio)/(bestvideo[height<=1080]+bestaudio)/best[height<=1080]";
-      cookies-from-browser = "chrome:${config.home.homeDirectory}/.var/app/com.google.Chrome/";
+      cookies-from-browser = "firefox";
     };
   };
 }
